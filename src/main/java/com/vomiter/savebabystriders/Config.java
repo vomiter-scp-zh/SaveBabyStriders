50e9108d497052f9d17519fcaba6fc1b5a4e9212
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-02-21 18:01:14 +0800

    1.1.0 forge

--- src/main/java/com/vomiter/savebabystriders/Config.java
+++ src/main/java/com/vomiter/savebabystriders/Config.java
@@ -1,10 +1,59 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.vomiter.savebabystriders;
 
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
-@Mod.EventBusSubscriber(modid = SaveBabyStriders.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config
-{
+@EventBusSubscriber(
+        modid = "savebabystriders",
+        bus = Bus.MOD
+)
+public class Config {
+    public static final ForgeConfigSpec COMMON_SPEC;
+    public static final Common COMMON;
+    public static boolean ADULT_STRIDER_LEAVES_MOUNT_ON_GROW = false;
+
+    public static class Common {
+        public final ForgeConfigSpec.BooleanValue adultStriderLeavesMountOnGrow;
+        public Common(ForgeConfigSpec.Builder builder) {
+            builder.push("general");
+            this.adultStriderLeavesMountOnGrow = builder.comment("If true, striders riding on other striders will automatically dismount when they become adults.").define("adultStriderLeavesMountOnGrow", false);
+            builder.pop();
+        }
+    }
+
+
+
+    @SubscribeEvent
+    public static void onLoad(ModConfigEvent.Loading event) {
+        if (event.getConfig().getSpec() == COMMON_SPEC) {
+            bake();
+        }
+
+    }
+
+    @SubscribeEvent
+    public static void onReload(ModConfigEvent.Reloading event) {
+        if (event.getConfig().getSpec() == COMMON_SPEC) {
+            bake();
+        }
+
+    }
+
+    private static void bake() {
+        ADULT_STRIDER_LEAVES_MOUNT_ON_GROW = COMMON.adultStriderLeavesMountOnGrow.get();
+    }
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        COMMON = new Common(builder);
+        COMMON_SPEC = builder.build();
+    }
 }
--- src/main/java/com/vomiter/savebabystriders/mixin/StriderMixin.java
+++ src/main/java/com/vomiter/savebabystriders/mixin/StriderMixin.java
@@ -1,12 +1,26 @@
 package com.vomiter.savebabystriders.mixin;
 
+import com.vomiter.savebabystriders.Config;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.Strider;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Strider.class)
-public class StriderMixin {
+public abstract class StriderMixin extends Animal {
+    @Unique
+    private boolean savebabystriders$wasBaby;
+
+    protected StriderMixin(EntityType<? extends Animal> p_27557_, Level p_27558_) {
+        super(p_27557_, p_27558_);
+    }
+
     @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/Strider;setSuffocating(Z)V"))
     private boolean saveStridersOnStriders(boolean flag){
         Strider $this = (Strider)(Object)this;
@@ -16,4 +30,28 @@ public class StriderMixin {
         }
         return flag;
     }
+
+    @Inject(
+            method = {"tick"},
+            at = {@At("HEAD")}
+    )
+    private void rememberBabyState(CallbackInfo ci) {
+        this.savebabystriders$wasBaby = isBaby();
+    }
+
+    @Inject(
+            method = {"tick"},
+            at = {@At("TAIL")}
+    )
+    private void savebabystriders$autoDismountOnGrow(CallbackInfo ci) {
+        if (!this.level().isClientSide) {
+            if (Config.ADULT_STRIDER_LEAVES_MOUNT_ON_GROW) {
+                if (this.savebabystriders$wasBaby && !this.isBaby() && this.getVehicle() instanceof Strider) {
+                    this.stopRiding();
+                }
+
+            }
+        }
+    }
+
 }
